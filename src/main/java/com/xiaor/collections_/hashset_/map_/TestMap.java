2eb5e4da1fb7dfce65c7e00164ab9a76cc069ab6
xiaoR <2471385107@qq.com>
2025-07-22 10:27:34 +0800

    feat(collections): - 添加 HashMap源码解析和测试用例 - 新增 HashMapSource 类，解析 HashMap 源码 - 新增 TestMap 类，演示 HashMap 的基本使用和遍历方式 - 通过示例代码和注释，详细解释了 HashMap 的内部结构和工作原理

---
+++ src/main/java/com/xiaor/collections_/hashset_/map_/TestMap.java
@@ -0,0 +1,29 @@
+package com.xiaor.collections_.hashset_.map_;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class TestMap {
+
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("xiaoR", "20240629");
+        map.put("qianqian", "20250702");
+
+
+        // 1. k-v最后是HashMap$Node node = newNode(hash, key, value, null);
+        // 2. 为了程序员遍历方便,map内部会创建一个EntrySet集合, EntrySet集合中存放的元素是Entry对象,
+        // 而Entry对象中保存的元素是k-v, 也就是EntrySet<Entry<k, v>> 即: transient Set<Map.Entry<K,V>> entrySet;
+        // 3. 在entrySet集合中, 定义的类型是Map.Entry<K,V>, 但实际上存放的还是HashMap$Node对象
+        //    这是因为static class Node<K, V> implements Map.Entry<K,V>
+        // 4. 当把HashMap$Node对象存放到entrySet就方便我们的遍历,因为Map.Entry提供了重要方法
+        //    K getKey(); V getValue();
+        Set set = map.entrySet();
+        System.out.println(set.getClass());
+        for (Object obj : set) {
+            Map.Entry entry = (Map.Entry) obj;
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+    }
+}
