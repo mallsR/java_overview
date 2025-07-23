944d39a4909501930d9711312a7199a44ee2c720
xiaoR <2471385107@qq.com>
2025-07-23 10:06:01 +0800

    test(TestCollections): 测试 Collections 类的方法
    - 添加了查找和替换相关方法的测试
    - 测试了 max 方法的使用
    -测试了自定义比较器的使用
    - 测试了 min 方法的使用
    - 测试了 frequency 方法的使用
    - 测试了 copy 方法的使用
    - 测试了 replaceAll 方法的使用

--- src/main/java/com/xiaor/collections_/TestCollections.java
+++ src/main/java/com/xiaor/collections_/TestCollections.java
@@ -1,6 +1,7 @@
 package com.xiaor.collections_;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -41,5 +42,38 @@ public class TestCollections {
         // swap: 交换List中指定位置的元素
         Collections.swap(list, 0, 1);
         System.out.println("交换后的list : " + list);
+
+
+        // 查找,替换
+        // max: 返回List中最大的元素
+        System.out.println("max : " + Collections.max(list));
+
+        // max(Collection, Comparator): 自定义比较器
+        System.out.println("max : " + Collections.max(list, new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((String)o1).length() - ((String)o2).length();
+            }
+        }));
+
+        // min: 跟max类似
+
+        // frequency: 返回指定元素在集合中出现的次数
+        System.out.println("xiaoR出现的次数 : " + Collections.frequency(list, "xiaoR"));
+
+        // copy: 将集合中的元素复制到数组中(保证目标集合元素空间不小于源集合)
+//        ArrayList desc = new ArrayList(list.size());  // 为何在初始化时无法分配指定大小的内存
+        ArrayList desc = new ArrayList();
+        for (int i = 0; i < list.size(); i++) {
+            desc.add(i);
+        }
+        System.out.println("desc.size(): " + desc.size());
+        System.out.println("list.size(): " + list.size());
+        Collections.copy(desc, list);
+        System.out.println("copy后的desc : " + desc);
+
+        // boolean replaceAll(List list, Object oldVal, Object newVal): 替换集合中所有元素为newVal
+        Collections.replaceAll(list, "xiaoR", "小R");
+        System.out.println("replaceAll后的list : " + list);
     }
 }
