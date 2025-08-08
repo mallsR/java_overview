22b5d283f1609fa63c71c1eb60732e69b8309909
xiaoR <2471385107@qq.com>
2025-08-08 17:16:50 +0800

    feat(udp): 实现 UDP聊天程序的双工通信
    - 在 UdpReceiverA 中添加了回复信息的功能
    - 在 UdpSenderB 中添加了接收信息的功能
    - 优化了代码结构，提高了可读性和可维护性

--- src/main/java/com/xiaor/stage3/network_/udp_/UdpSenderB.java
+++ src/main/java/com/xiaor/stage3/network_/udp_/UdpSenderB.java
@@ -15,9 +15,22 @@ public class UdpSenderB {
 
         DatagramPacket packet = new DatagramPacket(bytes, bytes.length, InetAddress.getByName("192.168.43.134"), 9999);
         socket.send(packet);
+        System.out.println("UdpSenderB 发送数据完毕!");
+
+        // 3. 接收信息
+        byte[] bytes1 = new byte[1024];
+        DatagramPacket packet1 = new DatagramPacket(bytes1, bytes1.length);
+
+        System.out.println("UdpSenderB 等待接收数据...");
+        socket.receive(packet1);
+
+        // 4. 把datagramPacket拆包,取出数据并显示
+        int length = packet1.getLength();    // 实际接收到的数据字节长度
+        byte[] data = packet1.getData();
+        String s = new String(data, 0, length);
+        System.out.println("B端接收数据为: " + s);
 
         // 3. 释放资源
         socket.close();
-        System.out.println("UdpSenderB 发送数据完毕!");
     }
 }
